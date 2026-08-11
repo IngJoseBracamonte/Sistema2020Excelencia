5e04393c56c4e107075137475fe168cca0e31c49
Jose Gregorio Bracamonte Gil <100859138+IngJoseBracamonte@users.noreply.github.com>
2026-08-11 13:28:57 -0400

    v4.0.6-rx-vs-tomo-domain-segregation-and-assertions

--- src/SistemaSatHospitalario.Core.Domain/Constants/TipoServicioConstants.cs
+++ src/SistemaSatHospitalario.Core.Domain/Constants/TipoServicioConstants.cs
@@ -7,9 +7,14 @@ namespace SistemaSatHospitalario.Core.Domain.Constants
         public const int Medico = 1;
         public const int Laboratorio = 2;
         public const int RX = 3;
+        public const int RayosX = 3;
         public const int Tomo = 4;
+        public const int Tomografia = 4;
         public const int Insumo = 5;
         public const int Informe = 6;
+
+        public const string RayosXString = "RX";
+        public const string TomografiaString = "TOMO";
 
         public const string CategoriaMedicamento = "Medicamento";
         public const string CategoriaInsumo = "Insumo";
